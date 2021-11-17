3c610109de3f2d1dc1568426e02b6cfa2f08c94f
Brice Friha <Brice.friha@outlook.com>
2021-11-17 16:02:45 +0000

    ping better

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,11 @@ import test from './testclass';
 import fetch from 'node-fetch';
 
 console.log(test.sayHello());
+
+// Ping the server
 fetch('https://api.coingecko.com/api/v3/ping')
-    .then(res => console.log(res));
+    .then(async (res) => {
+        const body = await res.json();
+
+        console.log(body);
+    });
